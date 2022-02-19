6c0f45e997dbb600c800915c0785e4198863feac
Bharath Chinthapanti <bharath@Bharaths-MacBook-Pro.local>
2022-02-19 18:03:35 -0500

    submisson

--- SEapp1/urls.py
+++ SEapp1/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("pages/", include("pages.urls")),
+    path('polls/', include('polls.urls')),
+   # path("pages/", include("pages.urls")),
 ]
--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,7 @@ from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
-    template_name = 'pages/index.html'
+    template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
@@ -17,19 +17,19 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'pages/detail.html'
+    template_name = 'polls/detail.html'
 
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'pages/results.html'
+    template_name = 'polls/results.html'
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'pages/detail.html', {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
         })
@@ -39,4 +39,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('pages:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
