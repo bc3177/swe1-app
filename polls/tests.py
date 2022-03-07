b9aa04e9a16b08e9228ba8c8c9b8cc81116c6638
bc3177 <90463880+bc3177@users.noreply.github.com>
2022-03-07 09:56:06 -0500

    Coveralls and Coverage

--- polls/tests.py
+++ polls/tests.py
@@ -10,7 +10,7 @@ class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
-        is in the future.
+        is in the future. Extra comment for the build.
         """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
